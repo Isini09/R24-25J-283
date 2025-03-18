fb511ffdd8f1e642bff13e1ee2d13f10858d84b3
Isini09 <shavindini19@gmail.com>
2025-03-18 12:02:01 +0530

    Final changes for 03/18/2025

--- frontend/src/pages/HarvestManagement/HarvestManagementPage.jsx
+++ frontend/src/pages/HarvestManagement/HarvestManagementPage.jsx
@@ -1,16 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Harvest = () => {
   const [showHarvest, setShowHarvest] = React.useState(true);
   const [showPrune, setShowPrune] = React.useState(false);
+
+  const getHarvestData = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/api/get-harvest-data/${blockId}`
+      );
+    } catch (error) {
+      console.error("Error fetching harvest data:", error);
+    }
+  };
+
+  const [blockId, setBlockId] = useState(" ");
+  const [harvestDate, setHarvestDate] = useState(" ");
+  const [pruneDate, setPruneDate] = useState(" ");
+
+  const postHarvestData = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/harvest-data",
+
+        { blockId, harvestDate }
+      );
+      alert(response.data.message);
+      window.location.reload();
+    } catch (error) {
+      alert(error.message);
+      console.error("Error post harvest data:", error);
+      window.location.reload();
+    }
+  };
+
+  const postPruneData = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/harvest-data",
+        { blockId, pruningDate: pruneDate }
+      );
+      alert(response.data.message);
+      window.location.reload();
+    } catch (error) {
+      alert(error.message);
+      console.error("Error posting prune data:", error);
+      window.location.reload();
+    }
+  };
+
+  const updateHarvestData = async () => {
+    try {
+      const response = await axios.put(
+        `http://localhost:5000/api/harvest-data/${blockId}`,
+        { blockId, harvestDate }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error updating harvest data:", error);
+    }
+  };
+
+  const updatePruneData = async () => {
+    try {
+      const response = await axios.put(
+        `http://localhost:5000/api/harvest-data/${blockId}`,
+        { blockId, pruneDate }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error updating harvest data:", error);
+    }
+  };
   return (
-    <div className="bg-[#111111] w-full h-screen">
+    <div className="bg-[#111111] h-screen">
       <div
         style={{
           display: "flex",
           flexDirection: "row",
           justifyContent: "center",
-       
         }}
       >
         <h1 style={{ fontSize: 50, color: "white" }}>Harvest Management</h1>
@@ -94,6 +163,7 @@ const Harvest = () => {
                     borderColor: "white",
                     color: "white",
                   }}
+                  onChange={(event) => setBlockId(event.target.value)}
                   type="text"
                 />
               </div>
@@ -118,7 +188,7 @@ const Harvest = () => {
                     color: "white",
                   }}
                   type="date"
-                  placeholder="Block"
+                  onChange={(event) => setHarvestDate(event.target.value)}
                 />
               </div>
               <div style={{ display: "flex", justifyContent: "center" }}>
@@ -132,7 +202,8 @@ const Harvest = () => {
                     borderColor: "white",
                     color: "white",
                   }}
-                  type="submit"
+                  type="button"
+                  onClick={postHarvestData}
                 >
                   Submit
                 </button>
@@ -174,6 +245,7 @@ const Harvest = () => {
                     color: "white",
                   }}
                   type="text"
+                  onChange={(event) => setBlockId(event.target.value)}
                 />
               </div>
 
@@ -197,7 +269,7 @@ const Harvest = () => {
                     color: "white",
                   }}
                   type="date"
-                  placeholder="Block"
+                  onChange={(event) => setPruneDate(event.target.value)}
                 />
               </div>
               <div style={{ display: "flex", justifyContent: "center" }}>
@@ -211,7 +283,8 @@ const Harvest = () => {
                     borderColor: "white",
                     color: "white",
                   }}
-                  type="submit"
+                  type="button"
+                  onClick={postPruneData}
                 >
                   Submit
                 </button>
